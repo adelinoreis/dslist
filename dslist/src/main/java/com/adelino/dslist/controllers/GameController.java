afa998d436bb453725aa37b899a4c74c0d4ba43e
adelinoreis <adelino.reis@outlook.com>
2025-05-25 10:30:53 -0300

    Game repository, service e controller

---
+++ dslist/src/main/java/com/adelino/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.adelino.dslist.controllers;
+
+import com.adelino.dslist.dto.GameMinDTO;
+import com.adelino.dslist.entities.Game;
+import com.adelino.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value = "/games")
+@RestController public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
